1141b50f6f043f9083706dd4b3d3c34de7a325cb
Igorurievich <igorurievich94@gmail.com>
2017-05-23 14:39:06 +0200

    Fully added Compressing test.

--- Bodia_benchmark_xamarin/Bodia_benchmark_xamarin/Activities/CompressFilesPerfomanceTestActivity.cs
+++ Bodia_benchmark_xamarin/Bodia_benchmark_xamarin/Activities/CompressFilesPerfomanceTestActivity.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using Android.App;
-using Android.OS;
+﻿using Android.OS;
 using Android.Views;
 using Android.Widget;
+using System;
 using System.IO;
-using Android.Content.Res;
+using System.IO.Compression;
 
 namespace Bodia_benchmark_xamarin.Sources
 {
-    [Activity]
-    public class CompressFilesPerfomanceTestActivity : Activity
+    [Android.App.Activity]
+    public class CompressFilesPerfomanceTestActivity : Android.App.Activity
     {
         TextView firstResultText;
         TextView secondResultText;
@@ -31,33 +30,111 @@ namespace Bodia_benchmark_xamarin.Sources
             thirdResultText = (TextView)FindViewById(Resource.Id.third_compressing_test_result);
             averageTimeResultText = (TextView)FindViewById(Resource.Id.arithmetic_main_compressing_test);
             btnRunCompressTest = (Button)FindViewById(Resource.Id.run_compress_test_button);
+            btnRunCompressTest.Click += BtnRunCompressTest_Click;
 
-            CopyFileOrDir("FilesForCompress");
+            CopyFilesForCompressFromAssets();
         }
 
-        private void CopyFileOrDir(string v)
+        private void BtnRunCompressTest_Click(object sender, EventArgs e)
         {
-            using (var assets = Assets.Open("FilesForCompress/file0"))
-            using (var dest = File.Create(Android.App.Application.Context.FilesDir.AbsolutePath, 65535, FileOptions.None))
-                assets.CopyTo(dest);
-        }
-        public static void CopyStream(Stream input, Stream output)
-        {
-            byte[] buffer = new byte[32768];
-            int read;
-            while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+            if (File.Exists(Globals.PathToCompressedZipFile))
             {
-                output.Write(buffer, 0, read);
+                File.Delete(Globals.PathToCompressedZipFile);
+            }
+            if (testNumber < 3)
+            {
+                double elapsedTime = EraseWhile();
+                testNumber++;
+                FillResults(testNumber, elapsedTime);
+            }
+            if (testNumber == 3)
+            {
+                double average = (Convert.ToDouble(firstResultText.Text) +
+                        Convert.ToDouble(secondResultText.Text) +
+                        Convert.ToDouble(thirdResultText.Text)) / 3;
+                averageTimeResultText.Text = average.ToString();
+                btnRunCompressTest.Text = Resources.GetString(Resource.String.start_new_tests_series_with_while_btn_label);
+                testNumber++;
+                return;
+            }
+            if (btnRunCompressTest.Text == Resources.GetString(Resource.String.start_new_tests_series_with_while_btn_label))
+            {
+                btnRunCompressTest.Text = Resources.GetString(Resource.String.label_button_run_while);
+                ClearTestsResults();
+                testNumber = 0;
             }
         }
 
-
-
-        private void CopyFile(object path)
+        private double EraseWhile()
         {
-            throw new NotImplementedException();
+            long tStart = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+            ZipFile.CreateFromDirectory(Globals.PathToFilesForCompressFolder, Globals.PathToCompressedZipFile);
+            try
+            {
+                ZipFile.ExtractToDirectory(Globals.PathToCompressedZipFile, Globals.PathToUncompressFilesFolder);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            long tEnd = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            long tDelta = tEnd - tStart;
+            File.Delete(Globals.PathToCompressedZipFile);
+            return tDelta / 1000.0;
         }
 
+        private void FillResults(int numberOfTest, double time)
+        {
+            switch (numberOfTest)
+            {
+                case 1:
+                    firstResultText.Text = Convert.ToString(time);
+                    break;
+                case 2:
+                    secondResultText.Text = Convert.ToString(time);
+                    break;
+                case 3:
+                    thirdResultText.Text = Convert.ToString(time);
+                    break;
+            }
+        }
 
+        private void ClearTestsResults()
+        {
+            firstResultText.Text = String.Empty;
+            secondResultText.Text = String.Empty;
+            thirdResultText.Text = String.Empty;
+            averageTimeResultText.Text = String.Empty;
+        }
+
+        private void CopyFilesForCompressFromAssets()
+        {
+            string[] filesForCompress = Assets.List(Globals.FilesForCompressFolderName);
+            if (Directory.Exists(Globals.PathToFilesForCompressFolder))
+            {
+                Directory.Delete(Globals.PathToFilesForCompressFolder, true);
+            }
+            Directory.CreateDirectory(Globals.PathToFilesForCompressFolder);
+            foreach (var file in filesForCompress)
+            {
+                byte[] buffer = ReadFully(Assets.Open(Path.Combine(Globals.FilesForCompressFolderName, file)));
+                File.WriteAllBytes(Path.Combine(Globals.PathToFilesForCompressFolder, file), buffer);
+            }
+        }
+
+        public static byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
     }
 }
--- Bodia_benchmark_xamarin/Core/Globals.cs
+++ Bodia_benchmark_xamarin/Core/Globals.cs
@@ -1,7 +1,17 @@
-﻿namespace Bodia_benchmark_xamarin
+﻿using System.IO;
+
+namespace Bodia_benchmark_xamarin
 {
     public static class Globals
     {
-        public static string UsersDatabaseName = "Users.db3";
+        public readonly static string UsersDatabaseName = "Users.db3";
+        public readonly static string FilesForCompressFolderName = "FilesForCompress";
+        public readonly static string UncompressedFilesFolderName = "UncompressedFiles";
+        public readonly static string CompressedFilesZipName = "CompressedFiles.zip";
+        public readonly static string ApplicationFilesFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+        public readonly static string PathToFilesForCompressFolder = Path.Combine(ApplicationFilesFolder, FilesForCompressFolderName);
+        public readonly static string PathToUncompressFilesFolder = Path.Combine(ApplicationFilesFolder, UncompressedFilesFolderName);
+        public readonly static string PathToCompressedZipFile = Path.Combine(ApplicationFilesFolder, CompressedFilesZipName);
+        
     }
 }
